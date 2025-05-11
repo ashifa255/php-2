bbb842410570994c15752fba5c7b6a29d66b24aa
ashifa255 <astammannahifa0330@gmail.com>
2025-05-11 18:21:46 +0600

    solve

--- quiz_app.php
+++ quiz_app.php
@@ -6,12 +6,12 @@ $option1 = "a) London\nb) Paris\nc) Berlin";
 $answer1 = "b";
 
 $question2 = "What is the color of the sky on a clear day?";
-$option1 = "a) Green\nb) Blue\nc) Red";
-$answer1 = "b";
+$option2 = "a) Green\nb) Blue\nc) Red";
+$answer2 = "b";
 
-$question1 = "What is 5 + 5?";
-$option1 = "a) 7\nb) 10\nc) 9";
-$answer1 = "b";
+$question3 = "What is 5 + 5?";
+$option3 = "a) 7\nb) 10\nc) 9";
+$answer3 = "b";
 
 $score = 0;
 
